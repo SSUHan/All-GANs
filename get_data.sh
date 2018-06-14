a20ca1e4944ee012e3738ea0abe9a2121ee8a6e3
Junsu Lee <ljs93kr@gmail.com>
2018-06-14 19:12:58 +0900

    get_data.sh added

---
+++ get_data.sh
@@ -0,0 +1,12 @@
+mkdir -p common/data/mnist
+mkdir -p common/data/fashion_mnist
+
+wget -P common/data/fashion_mnist http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz
+wget -P common/data/fashion_mnist http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz
+wget -P common/data/fashion_mnist http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz
+wget -P common/data/fashion_mnist http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz
+
+wget -P common/data http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget -P common/data http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget -P common/data http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget -P common/data http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
